84c68f88e9a508c46e0068ce6643714b6e000f81
“Bereket” <hsbereket@outlook.com>
2022-06-30 08:37:25 -0700

    correction git low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
+ * Return:always 0(Sucess)
  */
 int main(void)
 {
