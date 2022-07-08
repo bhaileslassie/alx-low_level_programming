85a7b5c9e30c5d3604c9516c4f59085eccfd9b8e
“Bereket” <hsbereket@outlook.com>
2022-07-08 13:21:38 -0700

    0x04

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,14 @@
-#include "_putchar.h"
+#include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
- */
+ *  * _isupper - a function that checks for uppercase character
+ *   * @c: input
+ *    * Return: 1 if c is uppercase, 0 otherwise
+ *     */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-
-    else
-        return (0);
+		if ((c >= 'A') && (c <= 'Z'))
+			return (1);
+		else
+			return (0);
 }
