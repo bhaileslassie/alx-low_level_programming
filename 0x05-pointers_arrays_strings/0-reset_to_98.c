92bfaa61ce06112d3e2ca1f9202d6e430ba8342b
“Bereket” <hsbereket@outlook.com>
2022-07-11 11:11:01 -0700

    0x05

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
 /**
- * _strcat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
- *
- * Return: address of dest
- */
-char *_strcat(char *dest, char *src)
+  * reset_to_98 - sets the pointer to 98.
+  * @n: pointer to change
+  */
+void reset_to_98(int *n)
 {
-	int i, j;
-
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
-	return (dest);
+	*n = 98;
 }
+
+
