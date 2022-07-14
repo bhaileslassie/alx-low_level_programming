611389aa8847562e7692c1cfc724c623dc3f5903
“Bereket” <hsbereket@outlook.com>
2022-07-14 12:02:05 -0700

    0x06

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * reverse_array - reverses the contents of any array
- * @a: integer array to be reversed
- * @n: length of array a
- *
- * Return: void
+ * reverse_array - reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
-    int i = 0, t;
+  int i, j, tmp;
 
-    n = n - 1;
-    while (i < n)
-    {
-        t = *(a + i);
-        *(a + i) = *(a + n);
-        *(a + n) = t;
-        i++;
-        n--;
-    }
+  j = n - 1;
+
+  for (i = 0; i < n / 2; i++)
+  {
+    tmp = a[i];
+    a[i] = a[j];
+    a[j--] = tmp;
+  }
 }
