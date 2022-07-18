967b75ba75d43201b2f6575f9829eb2675916d0d
“Bereket” <hsbereket@outlook.com>
2022-07-18 13:52:30 -0700

    0x07

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,31 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+
+char *_memset(char *s, char b, unsigned int n)
+
 {
-        unsigned int i;
+  unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+  /*Delacring FOR*/
+  for (i = 0; i < n; i++)
+  {
+    *(s + i) = b; /*add 1 position s*/
+
+  } /*END FOR*/
+
+  return (s);
 }
-
