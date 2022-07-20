02b069716c832d27ec6b642d675c5883e1605812
“Bereket” <hsbereket@outlook.com>
2022-07-20 16:11:07 -0700

    0x08

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 int strlen_no_wilds(char *str);
 void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
@@ -7,9 +6,8 @@ int wildcmp(char *s1, char *s2);
 
 /**
  * strlen_no_wilds - Returns the length of a string,
- *                   ignoring wildcard characters.
+ * ignoring wildcard characters.
  * @str: The string to be measured.
- *
  * Return: The length.
  */
 int strlen_no_wilds(char *str)
@@ -30,7 +28,7 @@ int strlen_no_wilds(char *str)
 
 /**
  * iterate_wild - Iterates through a string located at a wildcard
- *                until it points to a non-wildcard character.
+ * until it points to a non-wildcard character.
  * @wildstr: The string to be iterated through.
  */
 void iterate_wild(char **wildstr)
@@ -44,13 +42,12 @@ void iterate_wild(char **wildstr)
 
 /**
  * postfix_match - Checks if a string str matches the postfix of
- *                 another string potentially containing wildcards.
+ * another string potentially containing wildcards.
  * @str: The string to be matched.
  * @postfix: The postfix.
- *
  * Return: If str and postfix are identical - a pointer to the null byte
- *                                            located at the end of postfix.
- *         Otherwise - a pointer to the first unmatched character in postfix.
+ * located at the end of postfix.
+ * Otherwise - a pointer to the first unmatched character in postfix.
  */
 char *postfix_match(char *str, char *postfix)
 {
@@ -73,9 +70,8 @@ char *postfix_match(char *str, char *postfix)
  * wildcmp - Compares two strings, considering wildcard characters.
  * @s1: The first string to be compared.
  * @s2: The second string to be compared - may contain wildcards.
- *
  * Return: If the strings can be considered identical - 1.
- *         Otherwise - 0.
+ * Otherwise - 0.
  */
 int wildcmp(char *s1, char *s2)
 {
