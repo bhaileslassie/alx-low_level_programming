5a597541bc2912769fa9e1c3d2eed21de0de30cc
“Bereket” <hsbereket@outlook.com>
2022-07-20 17:00:45 -0700

    0x08

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,9 +10,9 @@
  */
 int palind2(char *a, int l)
 {
-    if (*a == 0)
-        return (l - 1);
-    return (palind2(a + 1, l + 1));
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
 }
 /**
  * palind3 - compares string vs string reverse
@@ -22,14 +22,13 @@ int palind2(char *a, int l)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int palind3(char *a, int l)
 {
-    if (*a != *(a + l))
-        return (0);
-    else if (*a == 0)
-        return (1);
-    return (palind3(a + 1, l - 2));
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
 }
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -40,8 +39,8 @@ int palind3(char *a, int l)
  */
 int is_palindrome(char *s)
 {
-    int l;
-
-    l = palind2(s, 0);
-    return (palind3(s, l));
+	int l;
+	
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
