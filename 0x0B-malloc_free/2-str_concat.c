0c7a0c9fe5225af75e67fd1d654ba83a93d6ee2d
“Bereket” <hsbereket@outlook.com>
2022-07-26 12:44:10 -0700

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,8 +21,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (n1 = 0; s1[n1] != '\0'; n1++)
-		;
+	for (n1 = 0; s1[n1] != '\0'; n1++);
 	
 	for (n2 = 0; s2[n2] != '\0'; n2++);
 	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
